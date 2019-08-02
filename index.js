4e21a08ad0da3bf660ec8e1f0fcad52e6a9da9f2
XRuiz9 <xdruiz92@gmail.com>
2019-08-02 09:35:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word) {
+  	return word[0].toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word) {
+  	return word.replace(/[^A-Za-z0-9-" "']+/g, '')
+  }
+
+  static titleize(sentence) {
+  	let notCap = [ "an", "but", "of", "and", "for", "at", "by", "from", "the", "a"]
+  	let split = sentence.split(" ")
+
+  	let result = split.map(function(word) {
+  		if (notCap.includes(word)) {
+  			return word
+  		}
+  		else {
+  			return Formatter.capitalize(word)
+  		}
+  	})
+
+  	return Formatter.capitalize(result.join(" "))
+
+  }
 }
